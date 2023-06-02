7f80ee384d395210bb4a482d998d703ce86a41ca
HHUUYYLLEE <lebahuybk@gmail.com>
2023-06-02 16:05:49 +0700

    employeelist

--- src/components/Employee/Employee.jsx
+++ src/components/Employee/Employee.jsx
@@ -1,25 +1,36 @@
 import React from 'react'
 import { BiUserCircle } from 'react-icons/bi'
 import { GiPositionMarker } from 'react-icons/gi'
-import { getAges } from '../../utils/utils'
+import { getAges, displayNum } from '../../utils/utils'
 export default function Employee({ employee }) {
   return (
     <div className='border-2 font-itim rounded-xl border-black mx-12 my-12'>
       <div className='flex ml-3 mt-2'>
-        <div className='mr-6'>
-          <BiUserCircle className='cursor-pointer' size={150} />
+        <div>
+          <div className='rounded-[50%] border-2 cursor-pointer overflow-hidden w-56 h-56 flex justify-center items-center'>
+            <img src='' alt='imageuser' />
+          </div>
         </div>
-        <div className='mr-3 w-full grid gap-x-16 gap-y-3'>
-          <div className='mt-2 col-span-1 cursor-pointer'>{employee.userID.name}</div>
-          <div className='flex -ml-1.5 mt-1 row-start-2 col-span-1'>
+
+        <div className='mx-3 flex-col gap-6'>
+          <div className='mt-2 cursor-pointer'>{employee.userID.name}</div>
+          <div className='flex -ml-1.5 mt-5'>
             <GiPositionMarker size={20} />
             <div className='-mt-1 font-bold text-3xl'>{employee.userID.address}</div>
           </div>
+          <div className='mt-5 flex'>
+            <div className='w-[16vh] text-ellipsis overflow-hidden whitespace-nowrap'>
+              年齢 : {getAges(employee.dateOB)}
+            </div>
+            <div className='w-[40vh] mr-[4vh] text-ellipsis overflow-hidden whitespace-nowrap'>
+              実験 : {employee.experience}
+            </div>
+            <div className='text-ellipsis overflow-hidden whitespace-nowrap'>
+              給料（円） : {displayNum(employee.salary)}
+            </div>
+          </div>
 
-          <div className='row-start-3 col-span-1'>年齢 : {getAges(employee.dateOB)}</div>
-          <div className='row-start-3 col-start-2 col-span-1'>実験 : {employee.experience}</div>
-          <div className='row-start-3 col-start-3 col-span-1'>給料 : {employee.salary}</div>
-          <div className='mb-12 line-clamp-2 row-start-4 col-span-4'>{employee.description}</div>
+          <div className='mt-5 mb-12 line-clamp-2'>{employee.description}</div>
         </div>
       </div>
     </div>
