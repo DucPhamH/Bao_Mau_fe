7846738b012b0326cde12a3fda967a2d350433dd
HHUUYYLLEE <lebahuybk@gmail.com>
2023-06-02 16:09:37 +0700

    employeelist

--- src/components/Employee/Employee.jsx
+++ src/components/Employee/Employee.jsx
@@ -4,16 +4,16 @@ import { GiPositionMarker } from 'react-icons/gi'
 import { getAges, displayNum } from '../../utils/utils'
 export default function Employee({ employee }) {
   return (
-    <div className='border-2 font-itim rounded-xl border-black mx-12 my-12'>
+    <div className='border-2 font-itim rounded-xl border-black mx-12 my-12 hover:shadow-2xl cursor-pointer hover:bg-[#ACFFFC]'>
       <div className='flex ml-3 mt-2'>
         <div>
-          <div className='rounded-[50%] border-2 cursor-pointer overflow-hidden w-56 h-56 flex justify-center items-center'>
+          <div className='rounded-[50%] border-2 cursor-pointer overflow-hidden w-56 h-56 flex justify-center items-center bg-white'>
             <img src='' alt='imageuser' />
           </div>
         </div>
 
         <div className='mx-3 flex-col gap-6'>
-          <div className='mt-2 cursor-pointer'>{employee.userID.name}</div>
+          <div className='mt-2'>{employee.userID.name}</div>
           <div className='flex -ml-1.5 mt-5'>
             <GiPositionMarker size={20} />
             <div className='-mt-1 font-bold text-3xl'>{employee.userID.address}</div>
