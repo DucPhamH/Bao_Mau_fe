a38ea760b8b3e74fb8bccafcbb8d34520ef451a9
HHUUYYLLEE <lebahuybk@gmail.com>
2023-06-07 02:12:39 +0700

    fix console error

--- src/page/Error404NotFound/Error404NotFound.jsx
+++ src/page/Error404NotFound/Error404NotFound.jsx
@@ -23,15 +23,17 @@ export default function Error404NotFound() {
       }
       audio.load()
       audio.volume = 0.6
-      audio.play()
+      setTimeout(function () {
+        audio.play()
+      }, 50)
     },
     { once: true }
   )
 
   return (
     <>
-      <div className=' bg-black cursor-default'>
-        <div className='w-[100vw] h-[100vh] flex justify-center items-center gap-9'>
+      <div className=' bg-black cursor-default overflow-hidden'>
+        <div className='w-[100vw] h-[90vh] flex justify-center items-center gap-9'>
           <div className='rounded-[50%] w-[30vw] h-[30vw] overflow-hidden flex justify-center items-center'>
             <div className='animation404 shadow-[0_0_20rem_rgba(255,255,255,1)] rounded-[50%] w-[30vw] h-[30vw] overflow-hidden relative flex justify-center items-center animate-entrance404first'>
               <div className='text-[#880808] z-10 text-[25vw] font-blooddrip mt-[10vw]'>4</div>
@@ -51,7 +53,7 @@ export default function Error404NotFound() {
             </div>
           </div>
         </div>
-        <div className='text-[#880808] text-[10vw] font-blooddrip mt-[-20vh]'>
+        <div className='text-[#880808] text-[10vw] font-blooddrip mt-[-10vh]'>
           <span className='animation404 animate-fadeIn1'>E</span>
           <span className='animation404 animate-fadeIn2'>R</span>
           <span className='animation404 animate-fadeIn3'>R</span>
