3aa64242b91a2e030cfc04d3de95f33349cc1cb0
DucPhamH <ducphamhong2@gmail.com>
2023-06-14 08:31:50 +0700

    add trang EmployeeAcceptedJobs

--- src/components/Post4/Post4.jsx
+++ src/components/Post4/Post4.jsx
@@ -61,24 +61,24 @@ const Post4 = ({ request }) => {
       className='flex flex-row items-center w-full border border-black rounded-2xl p-8 gap-8'
       style={{ boxShadow: '0px 4px 4px 0px #00000040' }}
     >
-      {/* <div className='rounded-[50%] border-2 cursor-pointer overflow-hidden w-[10vw] h-[10vw] flex justify-center items-center mb-20 ml-3'>
-        <img src={request.userID.image} alt='imageuser' />
+      <div className='rounded-[50%] border-2 cursor-pointer overflow-hidden w-[10vw] h-[10vw] flex justify-center items-center mb-20 ml-3'>
+        <img src={request.postID.userID.image} alt='imageuser' />
       </div>
       <div className='max-w-[60rem] ml-5'>
-        <div className='text-[20px]'>{request.title}</div>
+        <div className='text-[20px]'>{request.postID.title}</div>
         <div className='flex text-[20px] items-center'>
           <GiPositionMarker size={20} />
-          <div className='ml-1'>{request.address}</div>
+          <div className='ml-1'>{request.postID.address}</div>
         </div>
         <div className='flex gap-8 text-[16px] mt-4'>
-          <div>年齢: {request.age}</div>
-          <div>実験: {request.experience}</div>
-          <div>給料: {displayNum(request.salary)}</div>
+          <div>年齢: {request.postID.age}</div>
+          <div>実験: {request.postID.experience}</div>
+          <div>給料: {displayNum(request.postID.salary)}</div>
         </div>
 
-        <div className='text-[16px] mt-4'>勤務開始日: {convertDate(request.dateStart)}</div>
-        <div className='text-[16px] mt-4'>{request.description}</div>
-      </div> */}
+        <div className='text-[16px] mt-4'>勤務開始日: {convertDate(request.postID.dateStart)}</div>
+        <div className='text-[16px] mt-4'>{request.postID.description}</div>
+      </div>
       <div className='flex ml-30 flex-col gap-20 ml-auto'>
         <button className='bg-[#FA1313] mx-10 text-white text-[20px] p-1 rounded-[20px] w-[14rem] border-2 border-black shadow-[0_4px_0_rgb(0,0,0)] hover:shadow-[0_1px_0px_rgb(0,0,0)] ease-out hover:translate-y-1 transition-all'>
           キャンセル
--- src/page/EmployeeAcceptedJobs/EmployeeAcceptedJobs.jsx
+++ src/page/EmployeeAcceptedJobs/EmployeeAcceptedJobs.jsx
@@ -1,18 +1,16 @@
 import { useQuery } from '@tanstack/react-query'
-
-import Post3 from '../../components/Post3'
-import { getAllPostUserAccept } from '../../api/post.api'
 import Post4 from '../../components/Post4'
+import { getAcceptRequets } from '../../api/request.api'
 
 export default function EmployeeAcceptedJobs() {
-  //   const { data } = useQuery({
-  //     queryKey: ['requestAccept'],
-  //     queryFn: () => {
-  //       return getAllPostUserAccept()
-  //     }
-  //   })
-  //   const userRequest = data?.data.data
-  //   console.log(userRequest)
+  const { data } = useQuery({
+    queryKey: ['getRequestAccept'],
+    queryFn: () => {
+      return getAcceptRequets()
+    }
+  })
+  const userRequest = data?.data.data
+  console.log(userRequest)
   return (
     <div className='xl:w-[80%] rounded-2xl grid grid-cols-3 w-[95%] mx-64 gap-4 mt-24'>
       <div className='col-span-3 flex justify-center'>
@@ -20,13 +18,12 @@ export default function EmployeeAcceptedJobs() {
       </div>
       <div className='buttons-below col-span-3 mx-32  bg-white h-fit rounded-2xl mt-8 mb-16'>
         <div className='flex flex-col gap-20 p-8'>
-          {/* {userRequest &&
+          {userRequest &&
             userRequest.map((request) => (
               <div key={request._id}>
-                <Post3 request={request} />
+                <Post4 request={request} />
               </div>
-            ))} */}
-          <Post4 />
+            ))}
         </div>
       </div>
     </div>
