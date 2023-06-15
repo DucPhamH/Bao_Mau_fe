622baa9a1d96081d4bfeb94bb552871e6e2aef62
HHUUYYLLEE <lebahuybk@gmail.com>
2023-06-15 16:26:51 +0700

    contractedjobslist

--- src/page/ContractedJobsList/ContractedJobsList.jsx
+++ src/page/ContractedJobsList/ContractedJobsList.jsx
@@ -3,10 +3,10 @@ import ContractedJob from '../../components/ContractedJob/ContractedJob'
 export default function ContractedJobsList() {
   return (
     <div className='font-itim bg-[#DCEAFF] py-3'>
-      <div className='mx-16'>
+      <div className='mx-96'>
         <div className='bg-white rounded-xl text-center py-4 text-4xl'> Danh sách hoá đơn của tôi</div>
-        <div className='mt-16 bg-white rounded-xl'>
-          <div className='mx-9 py-8 h-[80rem] overflow-y-auto'>
+        <div className='mt-16 bg-white rounded-xl overflow-y-auto'>
+          <div className='mx-9 py-8 h-[80rem]'>
             <ContractedJob />
             <ContractedJob />
             <ContractedJob />
