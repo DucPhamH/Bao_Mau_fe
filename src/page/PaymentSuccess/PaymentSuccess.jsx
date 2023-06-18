8abc743f07176162f143308b388339c2b05ef6c9
HHUUYYLLEE <lebahuybk@gmail.com>
2023-06-19 01:02:52 +0700

    resize

--- src/page/PaymentSuccess/PaymentSuccess.jsx
+++ src/page/PaymentSuccess/PaymentSuccess.jsx
@@ -4,20 +4,20 @@ export default function PaymentSuccess() {
   return (
     <div className='relative'>
       <img className='absolute w-[100%] h-[100%] pointer-events-none z-0' src={money} alt='' />
-      <div className='flex justify-center items-center py-[4vw]'>
+      <div className='flex justify-center items-center pb-[4vw] pt-[1vw]'>
         <div className='rounded-[10rem] bg-white z-[1]'>
-          <div className='mx-[2vw] my-[5vw]'>
+          <div className='mx-[2vw] mb-[1.5vw] mt-[1vw]'>
             <div className='mx-[8vw]'>
               <div className='font-comfort italic text-green-600 bg-white text-[6vw]'>Payment success!</div>
               <div className='flex justify-center items-center'>
                 <img src={tick} alt='' />
               </div>
-              <div className='mt-[2vw] flex justify-center items-center font-playfair'>
+              <div className='mt-[1vw] flex justify-center items-center font-playfair'>
                 <div className='text-[6vw]'>-</div>
                 <div className='text-green-700 text-[6vw]'>$40.000</div>
               </div>
             </div>
-            <div className='mt-[3vw] flex justify-between font-itim'>
+            <div className='mt-[2vw] flex justify-between font-itim'>
               <button className='rounded-full px-[4vw] py-[1vw] text-white text-4xl bg-[#7101FF] hover:bg-yellow-500'>
                 View receipts
               </button>
