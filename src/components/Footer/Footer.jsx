f73d8e15a8948446767ce71a382e4db3c4a381f5
HHUUYYLLEE <lebahuybk@gmail.com>
2023-07-06 16:14:32 +0700

    test

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <div className='w-full h-[110px] flex items-center justify-between z-0 bg-[#CBDEE4] '>
       {/* <MessengerChat /> */}
-      <Timer />
+      {/* <Timer /> */}
 
       <div className='mx-24 text-4xl font-medium z-[2]'>著作権はSUKUNAに帰属します</div>
       <div className='mx-24 text-4xl font-medium z-[2]'>連絡先情報 : 012345678 </div>
--- src/page/UserProfile/UserProfile.jsx
+++ src/page/UserProfile/UserProfile.jsx
@@ -98,130 +98,131 @@ export default function UserProfile() {
   ///////////////////////
 
   //wait for api load
-  const [apiLoaded, setApiLoaded] = useState(false)
-  let interval = setInterval(function () {
-    let waitForAPICall = document.querySelector('.waitForAPICall')
-    if (waitForAPICall?.value !== '') {
-      setApiLoaded(true)
-      clearInterval(interval)
-    }
-  }, 100)
+  // const [apiLoaded, setApiLoaded] = useState(false)
+  // let interval = setInterval(function () {
+  //   let waitForAPICall = document.querySelector('.waitForAPICall')
+  //   if (waitForAPICall?.value !== '') {
+  //     setApiLoaded(true)
+  //     clearInterval(interval)
+  //   }
+  // }, 100)
   return (
-    <div className='w-full bg-[#DCEAFF]'>
-      <div className='mx-16 font-itim py-10'>
-        <div className={stateForm === 0 ? 'invisible' : ''}>
-          <div className='flex justify-center items-center'>
-            <button
-              onClick={backFunction}
-              className='px-[2vw] rounded-full text-white bg-red-600 hover:bg-green-600 py-3'
-            >
-              Back/Cancel
-            </button>
-          </div>
-        </div>
-        <div className='mt-10 grid gap-x-0 pb-24'>
-          <div className='bg-[#FFFFFF] rounded-3xl col-span-2 border-2 border-[#B9BFC9]'>
-            <div className='mb-8 mt-40 text-center'>
+    <>
+      {user && (
+        <div className='w-full bg-[#DCEAFF]'>
+          <div className='mx-16 font-itim py-10'>
+            <div className={stateForm === 0 ? 'invisible' : ''}>
               <div className='flex justify-center items-center'>
-                <div className='relative'>
-                  <div
-                    onClick={toggleModal}
-                    className='rounded-[50%] border-2 cursor-pointer overflow-hidden w-56 h-56 flex justify-center items-center'
-                  >
-                    <div className='absolute bottom-6 right-6 w-10 h-10 rounded-lg bg-white flex justify-center items-center'>
-                      <BsCameraFill />
+                <button
+                  onClick={backFunction}
+                  className='px-[2vw] rounded-full text-white bg-red-600 hover:bg-green-600 py-3'
+                >
+                  Back/Cancel
+                </button>
+              </div>
+            </div>
+            <div className='mt-10 grid gap-x-0 pb-24'>
+              <div className='bg-[#FFFFFF] rounded-3xl col-span-2 border-2 border-[#B9BFC9]'>
+                <div className='mb-8 mt-40 text-center'>
+                  <div className='flex justify-center items-center'>
+                    <div className='relative'>
+                      <div
+                        onClick={toggleModal}
+                        className='rounded-[50%] border-2 cursor-pointer overflow-hidden w-56 h-56 flex justify-center items-center'
+                      >
+                        <div className='absolute bottom-6 right-6 w-10 h-10 rounded-lg bg-white flex justify-center items-center'>
+                          <BsCameraFill />
+                        </div>
+                        <img src={user?.data?.image} alt='imageuser' />
+                      </div>
                     </div>
-                    <img src={user?.data?.image} alt='imageuser' />
+                  </div>
+                  <div className='bg-[#e8e7e74d] mx-36 mt-16 border-2 rounded-full text-center'>{user?.data.name}</div>
+                  <div className='mt-5 mx-36 border rounded-full text-center shadow-[inset_0px_4px_4px_0_rgb(0_0_0_/_0.1)] bg-[rgba(232,231,231,0.3)]'>
+                    {user?.data.phone}
+                  </div>
+
+                  <div className={stateForm === 0 ? '' : 'invisible'}>
+                    <button
+                      type='button'
+                      className='bg-[#FED5D5] px-8 rounded-full py-1 border border-black mt-10'
+                      onClick={() => changeStateForm(2)}
+                    >
+                      Change password
+                    </button>
+                  </div>
+                  <div className={stateForm === 2 ? '' : 'invisible'}>
+                    <ChangePass />
                   </div>
                 </div>
               </div>
-              <div className='bg-[#e8e7e74d] mx-36 mt-16 border-2 rounded-full text-center'>{user?.data.name}</div>
-              <div className='mt-5 mx-36 border rounded-full text-center shadow-[inset_0px_4px_4px_0_rgb(0_0_0_/_0.1)] bg-[rgba(232,231,231,0.3)]'>
-                {user?.data.phone}
-              </div>
-              <div className={apiLoaded ? '' : 'invisible'}>
-                <div className={stateForm === 0 ? '' : 'invisible'}>
-                  <button
-                    type='button'
-                    className='bg-[#FED5D5] px-8 rounded-full py-1 border border-black mt-10'
-                    onClick={() => changeStateForm(2)}
-                  >
-                    Change password
-                  </button>
+
+              <div className='bg-[#FFFFFF] rounded-3xl col-start-3 row-start-1 col-span-5 border-2 border-[#B9BFC9]'>
+                <div className='flex mt-9 mr-9 justify-end'>
+                  <div className={stateForm === 0 ? '' : 'invisible'}>
+                    <button
+                      type='button'
+                      className='bg-[#FED5D5] px-8 rounded-full py-1 border border-black'
+                      onClick={updateInfo}
+                    >
+                      Update
+                    </button>
+                  </div>
                 </div>
-              </div>
-              <div className={stateForm === 2 ? '' : 'invisible'}>
-                <ChangePass />
+                <form
+                  onSubmit={onSubmit}
+                  //  onChange={makeFormChangedStatus}
+                >
+                  <div className='ml-24 mr-32'>
+                    <div className='text-4xl'>プロフィールページ</div>
+                    <div className='grid gap-y-28 mt-32 ml-16'>
+                      <div className='col-span-1'>名前 :</div>
+                      <div className='col-start-2 col-span-2 py-1'>
+                        <input
+                          className='waitForAPICall updateinfo indent-10 w-full border rounded-xl border-black '
+                          disabled={stateForm === 1 ? false : true}
+                          name='name'
+                          autoComplete='on'
+                          {...register('name')}
+                        ></input>
+                        <div className='mt-1 flex min-h-[1.75rem] text-lg text-red-600'>{errors.name?.message}</div>
+                      </div>
+                      <div className='col-span-1 row-start-2'>メール :</div>
+                      <div className='col-start-2 col-span-2 py-1'>
+                        <input
+                          className='updateinfo indent-10 w-full border rounded-xl border-black'
+                          disabled={stateForm === 1 ? false : true}
+                          name='email'
+                          {...register('email')}
+                        ></input>
+
+                        <div className='mt-1 flex min-h-[1.75rem] text-lg text-red-600'>{errors.email?.message}</div>
+                      </div>
+                      <div className='col-span-1 row-start-3'>住所 :</div>
+                      <div className='col-start-2 col-span-2 py-1'>
+                        <input
+                          className='updateinfo indent-10 w-full border rounded-xl border-black'
+                          disabled={stateForm === 1 ? false : true}
+                          name='address'
+                          {...register('address')}
+                        ></input>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='my-24 flex justify-center'>
+                    <div className={stateForm === 1 ? '' : 'invisible'}>
+                      <button type='submit' className='bg-[#FED5D5] px-8 rounded-full py-1 border border-black'>
+                        Save
+                      </button>
+                    </div>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
-
-          <div className='bg-[#FFFFFF] rounded-3xl col-start-3 row-start-1 col-span-5 border-2 border-[#B9BFC9]'>
-            <div className='flex mt-9 mr-9 justify-end'>
-              <div className={apiLoaded ? '' : 'invisible'}>
-                <div className={stateForm === 0 ? '' : 'invisible'}>
-                  <button
-                    type='button'
-                    className='bg-[#FED5D5] px-8 rounded-full py-1 border border-black'
-                    onClick={updateInfo}
-                  >
-                    Update
-                  </button>
-                </div>
-              </div>
-            </div>
-            <form
-              onSubmit={onSubmit}
-              //  onChange={makeFormChangedStatus}
-            >
-              <div className='ml-24 mr-32'>
-                <div className='text-4xl'>プロフィールページ</div>
-                <div className='grid gap-y-28 mt-32 ml-16'>
-                  <div className='col-span-1'>名前 :</div>
-                  <div className='col-start-2 col-span-2 py-1'>
-                    <input
-                      className='waitForAPICall updateinfo indent-10 w-full border rounded-xl border-black '
-                      disabled={stateForm === 1 ? false : true}
-                      name='name'
-                      autoComplete='on'
-                      {...register('name')}
-                    ></input>
-                    <div className='mt-1 flex min-h-[1.75rem] text-lg text-red-600'>{errors.name?.message}</div>
-                  </div>
-                  <div className='col-span-1 row-start-2'>メール :</div>
-                  <div className='col-start-2 col-span-2 py-1'>
-                    <input
-                      className='updateinfo indent-10 w-full border rounded-xl border-black'
-                      disabled={stateForm === 1 ? false : true}
-                      name='email'
-                      {...register('email')}
-                    ></input>
-
-                    <div className='mt-1 flex min-h-[1.75rem] text-lg text-red-600'>{errors.email?.message}</div>
-                  </div>
-                  <div className='col-span-1 row-start-3'>住所 :</div>
-                  <div className='col-start-2 col-span-2 py-1'>
-                    <input
-                      className='updateinfo indent-10 w-full border rounded-xl border-black'
-                      disabled={stateForm === 1 ? false : true}
-                      name='address'
-                      {...register('address')}
-                    ></input>
-                  </div>
-                </div>
-              </div>
-              <div className='my-24 flex justify-center'>
-                <div className={stateForm === 1 ? '' : 'invisible'}>
-                  <button type='submit' className='bg-[#FED5D5] px-8 rounded-full py-1 border border-black'>
-                    Save
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
+          {modal && <UploadAvatar toggleModal={toggleModal} />}
         </div>
-      </div>
-      {modal && <UploadAvatar toggleModal={toggleModal} />}
-    </div>
+      )}
+    </>
   )
 }
